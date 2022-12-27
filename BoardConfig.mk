b823636c4bbab1b8fed1e12ea5a42bfe5a292264
JoKeRLeE <leejiangyee971027@gmail.com>
2022-12-27 23:45:55 +0800

    add back missing kernel cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,9 +50,12 @@ QCOM_BOARD_PLATFORMS += msmnile
 # Kernel
 BOARD_KERNEL_CMDLINE := \
 	console=ttyMSM0,115200n8 \
+	earlycon=msm_geni_serial,0xa90000 \
 	androidboot.hardware=qcom \
 	androidboot.console=ttyMSM0 \
 	androidboot.memcg=1 \
+	firmware_class.path=/vendor/firmware_mnt/image \
+	loop.max_part=7 \
 	lpm_levels.sleep_disabled=1 \
 	video=vfb:640x400,bpp=32,memsize=3072000 \
 	msm_rtb.filter=0x237 \
@@ -62,6 +65,7 @@ BOARD_KERNEL_CMDLINE := \
 	earlycon=msm_geni_serial,0xa90000 \
 	androidboot.usbconfigfs=true \
 	androidboot.boot_devices=soc/1d84000.ufshc \
+	androidboot.super_partition=system \
 	androidboot.selinux=permissive
 
 BOARD_KERNEL_BASE := 0x00000000
